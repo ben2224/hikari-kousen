d3ce1c6d9232d51ea7d89b18e60f96a583b79637
ben2224 <benpreece268@yahoo.com>
2021-09-20 18:34:48 +0100

    Added bot args - likely to move handling to StartedEvent

--- kousen/handler.py
+++ kousen/handler.py
@@ -19,34 +19,222 @@
 #  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 #  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 #  SOFTWARE.
-
+import asyncio
+import logging
 import typing as t
+from inspect import iscoroutinefunction
+
+import functools
 
 import hikari
 
 from kousen import Context, PartialContext
 
+__all__: list[str] = ["Bot"]
 
-__all__: list[str] = [
-    "Bot"
-]
+_LOGGER = logging.getLogger("kousen")
+
+
+def _handle_prefixes(prefix, bot_user_id=None):
+    prefix_list = []
+    if isinstance(prefix, str):
+        prefix_list.append(prefix)
+        if bot_user_id:
+            prefix_list.extend((f"<@{bot_user_id}>", f"<@!{bot_user_id}>"))
+        return functools.partial(_base_getter, return_object=prefix_list)
+
+    if isinstance(prefix, t.Iterable):
+        prefix_list.extend(list(map(str, prefix)))
+        if bot_user_id:
+            prefix_list.extend((f"<@{bot_user_id}>", f"<@!{bot_user_id}>"))
+        return functools.partial(_base_getter, return_object=prefix_list)
+
+    if iscoroutinefunction(prefix):
+        return functools.partial(
+            _prefix_getter_with_callback, callback=prefix, bot_id=bot_user_id
+        )
+
+    else:
+        raise TypeError(
+            f"Prefix must be either a string, or iterable of strings, or a coroutine, not type {type(prefix)}"
+        )
+
+
+async def _prefix_getter_with_callback(
+    partial_context: PartialContext, callback, bot_id
+):
+    getter_result = await callback(partial_context)
+    prefixes = []
+
+    if isinstance(getter_result, str):
+        prefixes.append(str)
+        if bot_id:
+            prefixes.extend((f"<@{bot_id}>", f"<@!{bot_id}>"))
+        return prefixes
+
+    if isinstance(getter_result, t.Iterable):
+        prefixes.extend(list(map(str, getter_result)))
+        if bot_id:
+            prefixes.extend((f"<@{bot_id}>", f"<@!{bot_id}>"))
+        return prefixes
+
+    else:
+        raise TypeError(
+            f"The prefix callback must return a string or iterable of strings, not type {type(getter_result)}"
+        )
+
+
+def _base_bool_getter_handler(callable_or_bool, name: str):
+    if isinstance(callable_or_bool, bool):
+        return functools.partial(_base_getter, return_object=callable_or_bool)
+    elif iscoroutinefunction(callable_or_bool):
+        return functools.partial(
+            _base_getter_with_callback,
+            callback=callable_or_bool,
+            result_type=bool,
+            error_text=f"The {name} callback must return a bool",
+        )
+    else:
+        raise TypeError(
+            f"{name.capitalize()} arg must be either a bool or a coroutine, not type {type(callable_or_bool)}"
+        )
+
+
+async def _base_getter_with_callback(
+    partial_context_or_context, callback, result_type, error_text
+):
+    getter_result = await callback(partial_context_or_context)
+
+    if isinstance(getter_result, result_type):
+        return getter_result
+    else:
+        raise TypeError(f"{error_text}, not type {type(getter_result)}")
+
+
+async def _base_getter(_, return_obj):
+    return return_obj
 
 
 class Bot(hikari.GatewayBot):
+    """The standard implementation of :class:`hikari.impl.bot.GatewayBot` with a command handler.
+
+    Note
+    ----
+    Mention prefixes will continue to work with message commands after the message content intent becomes privileged.
+
+    Parameters
+    ----------
+    default_prefix : Union[:obj:`str`, Iterable[:obj:`str`], Callable[[:obj:`~PartialContext`], Coroutine[:obj:`None`, :obj:`None`, Union[:obj:`str`, Iterable[:obj:`str`]]]]
+        The bot's command prefix.
+    mention_prefix : :obj:`bool`
+        Whether or not the bot's mention will be used as a prefix. This will be `True` if no default
+        prefix is provided, else `False` by default.
+
+    Other Parameters
+    ----------------
+    default_parser : Union[:obj:`str`, Callable[[:obj:`~PartialContext`], Coroutine[:obj:`None`, :obj:`None`, :obj:`str`]]]
+        The default parser to use for parsing message content for command arguments. Defaults to a whitespace.
+    weak_command_search : :obj:`bool`
+        If `True`, then the bot will parse for the prefix throughout the message content (instead of just the
+        start), allowing for commands to be called at any point in the message. Defaults to False.
+    case_insensitive_commands : :obj:`bool`
+        Whether or not commands should be case-insensitive or not. Defaults to `False` (commands are case-sensitive).
+    case_insensitive_prefixes : :obj:`bool`
+        Whether or not prefixes should be handled as case-insensitive or not.
+        Defaults to `False` (prefixes are case-sensitive).
+    ignore_bots : :obj:`bool`
+        Prevents other bot's messages invoking your bot's commands if `True`. Defaults to `True`.
+    owners : Iterable[`SnowFlakeishOr[hikari.User]`]
+        The IDs or User objects of the users which should be treated as "owners" of the bot.
+        By default this will include the bot owner's id.
+    """
 
     __slots__ = ()
 
     def __init__(
         self,
         *,
-        global_prefix: t.Union[str, t.Sequence[str], t.Callable[[PartialContext], t.Coroutine[t.Any, t.Any, t.Union[str, t.Sequence[str]]]]] = None,
+        default_prefix: t.Union[
+            str,
+            t.Iterable[str],
+            t.Callable[
+                [PartialContext], t.Coroutine[None, None, t.Union[str, t.Iterable[str]]]
+            ],
+        ] = None,
         mention_prefix: bool = False,
-        global_parser: t.Union[str, t.Callable[[Context], t.Coroutine[t.Any, t.Any, str]]] = " ",
-        strict_command_search: bool = True,
-        insensitive_commands: bool = False,
-        insensitive_prefixes: bool = False,
-        ignore_bots: bool = True,
-        owner_ids: t.Sequence[int] = None,
-        **kwargs
+        default_parser: t.Union[
+            str, t.Callable[[Context], t.Coroutine[None, None, str]]
+        ] = " ",
+        weak_command_search: t.Union[
+            bool, t.Callable[[PartialContext], t.Coroutine[None, None, bool]]
+        ] = False,
+        case_insensitive_commands: t.Union[
+            bool, t.Callable[[PartialContext], t.Coroutine[None, None, bool]]
+        ] = False,
+        case_insensitive_prefixes: t.Union[
+            bool, t.Callable[[PartialContext], t.Coroutine[None, None, bool]]
+        ] = False,
+        ignore_bots: t.Union[
+            bool, t.Callable[[PartialContext], t.Coroutine[None, None, bool]]
+        ] = True,
+        owners: t.Iterable[hikari.SnowflakeishOr[hikari.User]] = (),
+        **kwargs,
     ) -> None:
         super().__init__(**kwargs)
+        if (cache_settings := kwargs.get("cache_settings")) is not None:
+            self._cache_components = cache_settings.components
+        else:
+            self._cache_components = hikari.CacheComponents.ALL
+
+        if mention_prefix is False and default_prefix is None:
+            raise ValueError(
+                "No default prefix was provided and mention_prefix was set to False."
+            )
+        user = None
+        if mention_prefix is True or default_prefix is None:
+            user = self.get_me()
+            # todo implement backoff with fetch and do in startedevent as rest/cache doesn't exist yet
+            # if not user:
+            #   user = await self.rest.fetch_my_user()
+        self._prefix_getter = _handle_prefixes(default_prefix, user)
+
+        if isinstance(default_parser, str):
+            self._default_parser_getter = functools.partial(
+                _base_getter, return_object=default_parser
+            )
+        elif iscoroutinefunction(default_parser):
+            self._default_parser_getter = functools.partial(
+                _base_getter_with_callback,
+                callback=default_parser,
+                result_type=str,
+                error_text="The parser callback must return a string",
+            )
+        else:
+            raise TypeError(
+                f"Parser must be either a string or a coroutine, not type {type(default_parser)}"
+            )
+
+        self._weak_command_search = _base_bool_getter_handler(
+            weak_command_search, "Weak command search"
+        )
+        self._case_insensitive_commands = _base_bool_getter_handler(
+            case_insensitive_commands, "Case insensitive commands"
+        )
+        self._case_insensitive_prefixes = _base_bool_getter_handler(
+            case_insensitive_prefixes, "Case insensitive prefixes"
+        )
+        self._ignore_bots = _base_bool_getter_handler(ignore_bots, "Ignore bots")
+
+        if not isinstance(owners, t.Iterable):
+            raise TypeError(f"Owners must be an iterable, not type {type(owners)}")
+        else:  # todo if owners is None then append bot owner's id
+            self._owners = []
+            for owner in owners:
+                if isinstance(owner, int):
+                    self._owners.append(int(owner))
+                elif isinstance(owner, hikari.User):
+                    self._owners.append(int(owner.id))
+                else:
+                    raise TypeError(
+                        f"Owners must be an iterable of Snowflakeish type or hikari.User, not type {type(owner)}"
+                    )
