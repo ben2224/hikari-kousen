561eb56e6c2bc462a04dd2ab66a71c701f8b21e9
ben2224 <benpreece268@yahoo.com>
2021-10-03 21:33:59 +0100

    setup setup.py

--- setup.py
+++ setup.py
@@ -19,3 +19,48 @@
 #  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 #  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 #  SOFTWARE.
+from setuptools import setup, find_namespace_packages
+
+
+def long_description():
+    with open("README.md") as fp:
+        return fp.read()
+
+
+def parse_requirements_file(path):
+    with open(path) as fp:
+        dependencies = (d.strip() for d in fp.read().split("\n") if d.strip())
+        return [d for d in dependencies if not d.startswith("#")]
+
+
+setup(
+    name="hikari-kousen",
+    version="0.0.1",
+    description="A command handler for Hikari",
+    long_description=long_description(),
+    long_description_content_type="text/markdown",
+    author="ben2224",
+    author_email="thatbenn@gmail.com",
+    url="https://github.com/ben2224/hikari-kousen",
+    packages=find_namespace_packages(include=["kousen*"]),
+    license="MIT",
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=parse_requirements_file("requirements.txt"),
+    python_requires=">=3.8.0,<=3.10",
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Framework :: AsyncIO",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Software Development :: Libraries :: Application Frameworks",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+)
