728d664e7ca27fc78ee9c87977ecd1e5207944f8
ben2224 <benpreece268@yahoo.com>
2021-11-27 12:16:31 +0000

    created base impl for options and parser

--- kousen/parsing.py
+++ kousen/parsing.py
@@ -21,24 +21,70 @@
 #  SOFTWARE.
 from __future__ import annotations
 import typing as t
+import re
+
+import hikari
+from hikari.internal.enums import Enum
 
 if t.TYPE_CHECKING:
     ...
 
-__all__: list[str] = ["with_slash_parser", "with_option"]
+__all__: list[str] = ["Option", "ParserManager", "with_option"]
 
 
-def with_slash_parser():
-    ...
+class OptionType(int, Enum):
+    STRING = 3
+    INTEGER = 4
+    BOOLEAN = 5
+    USER = 6
+    CHANNEL = 7
+    ROLE = 8
+    MENTIONABLE = 9
+    FLOAT = 10
+
+
+class Option:
+
+    __slots__ = ("_name", "_description", "_option_type", "_converter", "_default")
+
+    def __init__(self, name: str, description: str, option_type: OptionType, converter, default: t.Any = hikari.UNDEFINED):
+        if not re.match(r"^[a-z0-9_-]{1,32}$", str(name)):
+            raise  # todo log error
+        if len(description) > 100 or len(description) < 1:
+            raise  # todo log error
+        if not isinstance(option_type, OptionType):
+            raise  # todo log error
+        self._name: str = name
+        self._description: str = description
+        self._option_type: OptionType = option_type
+        self._converter = converter
+        self._default: t.Any = default
+
+    def convert(self, original) -> t.Any:
+        ...
+
+
+class ParserManager:
+
+    __slots__ = ("_names_to_options",)
+
+    def __init__(self):
+        self._names_to_options: dict[str, Option] = {}
+
+    def convert_from_string(self, text: str) -> dict[str, t.Any]:
+        # returns mapping of option name to the converted value
+        ...
+
+    def convert_from_options(self, *options) -> dict[str, t.Any]:
+        ...
 
 
 def with_option(
     name: str,
     description: str,
     *,
-    option_type,
+    option_type: OptionType = OptionType.STRING,
     converter=None,
-    keyword_name: t.Optional[str] = None,
-    default: t.Optional[t.Any] = None
+    default: t.Any = hikari.UNDEFINED
 ):
     ...
